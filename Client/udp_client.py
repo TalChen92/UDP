95d4b22ea40c28510dcc3ea869afdda23e07b17c
Tal Chen <talchen92@gmail.com>
2025-02-16 19:13:45 +0200

    v1

--- Client/udp_client.py
+++ Client/udp_client.py
@@ -1,7 +1,11 @@
 import socket
-
+#socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('127.0.0.1', 12345)
 
 client_socket.sendto(b"Hello, Server!", server_address)
 client_socket.sendto(b"Another message", server_address)
+
+if __name__ == "__main__":
+    print("hello")
+    print("add")
